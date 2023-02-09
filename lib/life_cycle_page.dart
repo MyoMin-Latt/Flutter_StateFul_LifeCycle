3741ee6d1bb801998d358d72d6378d93c9b67f97
MyoMinLatt <myominlatt.sbs@gmail.com>
2023-02-09 09:25:07 +0630

    flutter lifecycle with count number

---
+++ lib/life_cycle_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key, required this.number});
+  final int number;
+
+  @override
+  // ignore: no_logic_in_create_state
+  State<MyWidget> createState() {
+    debugPrint('No: $number createState');
+
+    return _MyWidgetState();
+  }
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int number = 0;
+  @override
+  void initState() {
+    super.initState();
+    debugPrint('No: ${widget.number} initState');
+    number = widget.number;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    debugPrint('No: ${widget.number} didChangeDependencies');
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    debugPrint('No: ${widget.number} deactivate');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    debugPrint('No: ${widget.number} dispose');
+  }
+
+  @override
+  void didUpdateWidget(covariant MyWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    debugPrint('No: ${widget.number} didUpdateWidget');
+    if (oldWidget.number != widget.number) {
+      number = widget.number;
+      debugPrint('Number has changed');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('No: ${widget.number} build');
+    return TextButton(
+        onPressed: () {
+          debugPrint('No: ${widget.number} setState');
+
+          setState(() {
+            number += 1;
+          });
+        },
+        child: Text(
+          number.toString(),
+          //  widget.number.toString(),
+          style: const TextStyle(fontSize: 50),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stateful_cycle/life_cycle_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 1;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // body: ListView.builder(
+      //   itemCount: 100,
+      //   itemBuilder: (context, index) =>
+      //       SizedBox(height: 500, child: MyWidget(number: index)),
+      // ),
+      body: Center(
+        child: MyWidget(number: _counter),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
